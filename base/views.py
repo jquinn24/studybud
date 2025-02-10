7f48e5218b9c45b7fa063760fa44a1402c34d7ec
[jquinn24] <[jquinn24@charlotte.edu]>
2025-02-09 23:42:30 -0500

    2:22:27 - User Login

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room,Topic
 from.forms import RoomForm
+from django.db.models import Q 
+
 
 
 
@@ -11,14 +13,23 @@ from.forms import RoomForm
 #]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html',{'rooms':rooms})
+    q= request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q)
+     | Q(name__icontains=q)
+     | Q(description__icontains=q)
+     )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html',context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     
-    context = {'room': room}
+    context = {'room': room, 'topic': room.topic}
     return render(request, 'base/room.html', context)
 
 def createRoom(request):
