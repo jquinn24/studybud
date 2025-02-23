e3f7b427b5e3c2b398135c5e08e89e917a859a56
[jquinn24] <[jquinn24@charlotte.edu]>
2025-02-23 02:00:28 -0500

    5:53:00 - Django REST Framework

--- base/urls.py
+++ base/urls.py
@@ -19,4 +19,8 @@ urlpatterns = [
     path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
 
     path('update-user/', views.updateUser, name='update-user'),
+
+    path('topics/', views.topicsPage, name='topics'),
+
+    path('activity/', views.activtyPage, name='activity'),
 ]
