e8c8704054fd4c15c88b5beaff1dae7a5a1992cd
[jquinn24] <[jquinn24@charlotte.edu]>
2025-02-22 23:11:21 -0500

    5:11:50 - Edit User Account Page

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
 
     path('', views.home, name='home'),
-    path('room/<str:pk>', views.room, name='room'),
+    path('room/<str:pk>/', views.room, name='room'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     
 
--- base/views.py
+++ base/views.py
@@ -108,15 +108,25 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
         form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
 
-    context = {'form': form}
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+        #if form.is_valid():
+        #   room = form.save(commit=False)
+        #   room.host = request.user
+        #   room.save()
+        #    return redirect('home')
+
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -129,12 +139,15 @@ def updateRoom(request, pk):
         return HttpResponse('You are not allowed here!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.descriptions = request.POST.get('description')
+        
+        return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form,'topics': Topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 
